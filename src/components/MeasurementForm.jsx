38bd7ea149f2e523dfe68ec4c60af78cf3810ba1
Tabita <taby_marcu@yahoo.com>
2020-11-16 22:56:27 +0200

    Started to style Dashboard Page

--- src/components/MeasurementForm.jsx
+++ src/components/MeasurementForm.jsx
@@ -3,6 +3,11 @@ import firebase, { auth } from 'firebase/app'
 import date from '../utils/utils'
 import useFetch from '../service/useFetch'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 export default function MeasurementForm() {
   const [measurement, setMesurement] = useState({
@@ -69,56 +74,65 @@ export default function MeasurementForm() {
   }
 
   return (
-    <div>
-      <h1> Dashboard page</h1>
+    <ContainerWrapper>
+      <Title> your measurements.</Title>
       <div>
-        <input
-          type="text"
-          placeholder="waist"
-          name="waist"
-          value={measurement.waist}
-          onChange={(event) => onChangeHandler(event)}
-        />
+        <Input>
+          <InputData
+            type="text"
+            placeholder="waist"
+            name="waist"
+            value={measurement.waist}
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </Input>
         {isNaN(measurement.waist) ? 'not a number' : ''}
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="bicep"
-          name="bicep"
-          value={measurement.bicep}
-          onChange={(event) => onChangeHandler(event)}
-        />
+
+        <Input>
+          <InputData
+            type="text"
+            placeholder="bicep"
+            name="bicep"
+            value={measurement.bicep}
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </Input>
         {isNaN(measurement.bicep) ? 'not a number' : ''}
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="chest"
-          name="chest"
-          value={measurement.chest}
-          onChange={(event) => onChangeHandler(event)}
-        />
+
+        <Input>
+          <InputData
+            type="text"
+            placeholder="chest"
+            name="chest"
+            value={measurement.chest}
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </Input>
         {isNaN(measurement.chest) ? 'not a number' : ''}
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="weight"
-          name="weight"
-          value={measurement.weight}
-          onChange={(event) => onChangeHandler(event)}
-        />
+
+        <Input>
+          <InputData
+            type="text"
+            placeholder="weight"
+            name="weight"
+            value={measurement.weight}
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </Input>
         {isNaN(measurement.weight) ? 'not a number' : ''}
-      </div>
-      <div>
-        <button type="submit" onClick={addMeasurement}>
+
+        <AddMeasurementButton
+          variant="light"
+          type="submit"
+          onClick={addMeasurement}
+        >
           Add measure
-        </button>
+        </AddMeasurementButton>
       </div>
 
       <div>
-        <button
+        <LogoutButton
+          variant="light"
           onClick={() => {
             firebase.auth().signOut()
             localStorage.removeItem('user')
@@ -127,8 +141,39 @@ export default function MeasurementForm() {
           }}
         >
           Logout
-        </button>
+        </LogoutButton>
       </div>
-    </div>
+    </ContainerWrapper>
   )
 }
+
+const ContainerWrapper = styled(Container)``
+
+const Title = styled.p`
+  text-align: center;
+  font-size: 25px;
+  margin: 70px 0 100px 0;
+`
+
+const LogoutButton = styled(Button)`
+  position: absolute;
+  bottom: 40px;
+  right: 40px;
+  background-color: #65656b;
+  border-color: #65656b;
+`
+
+const Input = styled(InputGroup)`
+  width: 20%;
+  margin-bottom: 1em;
+  margin-left: -3em;
+`
+const InputData = styled(FormControl)`
+  background-color: #9999a0;
+  border-color: #9999a0;
+`
+
+const AddMeasurementButton = styled(Button)`
+  border-color: #f3591e;
+  background-color: #f3591e;
+`
